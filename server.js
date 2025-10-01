52a9cc22119e30bce3864f634d1de5da21f4490b
araume <95178210+araume@users.noreply.github.com>
2025-10-02 00:21:05 +0800

    Add find by ID product

--- server.js
+++ server.js
@@ -8,19 +8,31 @@ import Product from "./models/product.model.js";
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.send("Hello from NodeJS server API");
 });
 
-app.get("/api/products", async (req, res) => {
+app.get('/api/products', async (req, res) => {
   try {
     const products = await Product.find({});
+    res.status(200).json(products);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-app.post("/api/products", async (req, res) => {
+app.get('/api/product/:id', async (req, res) => {
+  try{
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  }
+  catch(error){
+    res.status(500).json({message: error.message});
+  }
+});
+
+app.post('/api/products', async (req, res) => {
   try {
     const product = await Product.create(req.body);
     res.status(200).json(product);
